3fef247d4536ef397a4f8c43ddc04da56826307c
KinXpeng <kinxpeng@163.com>
2022-08-12 10:25:47 +0800

    update:docs

--- docs/assets/a2.62b2e70a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index.95851a34.js";const o={};function r(t,a){return n(),c("div",null,"a2")}const _=e(o,[["render",r]]);export{_ as default};
--- docs/assets/b1.f8583c0a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index.95851a34.js";const o={};function r(t,s){return n(),c("div",null,"b1")}const a=e(o,[["render",r]]);export{a as default};
